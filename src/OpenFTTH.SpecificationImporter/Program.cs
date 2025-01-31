352e5e239fab79e1cf4bbe46bcb4e238923e85ee
runeanielsen <runenielsen@runbox.com>
2025-01-31 09:49:33 +0100

    setup serilog

--- src/OpenFTTH.SpecificationImporter/Program.cs
+++ src/OpenFTTH.SpecificationImporter/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace OpenFTTH.SpecificationImporter;
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace OpenFTTH.SpecificationImporter;
 
 internal static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        var logger = GetLogger();
+        logger.Information("Hello, World!");
+    }
+
+    private static Logger GetLogger()
+    {
+        return new LoggerConfiguration()
+            .MinimumLevel.Override("System", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .WriteTo.Console(new CompactJsonFormatter())
+            .CreateLogger();
     }
 }
